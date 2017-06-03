5e3135094dc2629b421060bea1dbd0101b77ca38
Jessica Prado <jessica.marie.prado@gmail.com>
2017-06-03 10:09:40 -0400

    basic socket up and running. Chat works

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 var express = require('express');
-var path = require('path');
 var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
-var port = 8000;
-
-app.use(express.static(path.join(__dirname, "public")));
-
-io.on('connection', function(socket) {
-  console.log('new connection made');
-
-});
+user = [];
+connections = [];
+var port = 3000;
 
 server.listen(port, function() {
   console.log("Listening on port " + port);
 });
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html')
+})
+
+io.sockets.on('connection', function(socket){
+	connections.push(socket);
+	console.log('Connected: %s sockets connected', connections.length);
+
+	//disconnect
+	socket.on('disconnect', function(data){
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Disonnected: %s sockets connected', connections.length);
+	}) 
+
+	//send Message
+	socket.on('send message', function(data){
+		console.log(data);
+		io.sockets.emit('new message', {msg:data});
+	})
+	
+})
