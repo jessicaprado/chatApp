209f2b02425ffa8ce6041f173e41a54e63764020
Jessica Prado <jessica.marie.prado@gmail.com>
2017-06-06 11:33:08 -0400

    broadcast works

--- public/chat.js
+++ public/chat.js
@@ -6,6 +6,7 @@ var message = document.getElementById('message');
 var handle = document.getElementById('handle');
 var btn = document.getElementById('send');
 var output = document.getElementById('output');
+var feedback = document.getElementById('feedback');
 
 btn.addEventListener('click', function(){
 	socket.emit('chat', {
@@ -14,7 +15,15 @@ btn.addEventListener('click', function(){
 	})
 })
 
+message.addEventListener('keypress', function () {
+	socket.emit('typing', handle.value);
+})
+
 //Listen for Events
 socket.on('chat', function(data){
 	output.innerHTML += '<p><strong>' + data.handle + ':</strong> ' + data.message + '</p>'
+})
+
+socket.on('typing', function (data) {
+	feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>'
 })
